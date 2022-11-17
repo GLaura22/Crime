ee313cf8b75554a8f7f0b09dd0642b6ebf6a62cb
GLaura22 <laura.gergelyi@gmail.com>
2022-11-17 10:10:18 +0100

    Add project files.

---
+++ Crime/Crime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crime
+{
+    public class Crime
+    {
+        public string type;
+        public string year, month, day, hour, minute;
+        public string hundred_block, neighbourhood;
+        public string x, y;
+        public string latitude, longitude;
+
+        public Crime(string type, string year, string month, string day, string hour, string minute, string hundred_block, string neighbourhood, string x, string y, string latitude, string longitude)
+        {
+            this.type = type;
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hour = hour;
+            this.minute = minute;
+            this.hundred_block = hundred_block;
+            this.neighbourhood = neighbourhood;
+            this.x = x;
+            this.y = y;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public Crime()
+        {
+            this.type = "";
+            this.year = "";
+            this.month = "";
+            this.day = "";
+            this.hour = "";
+            this.minute = "";
+            this.hundred_block = "";
+            this.neighbourhood = "";
+            this.x = "";
+            this.y = "";
+            this.latitude = "";
+            this.longitude = "";
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11}", type, year, month, day, hour, minute, hundred_block, neighbourhood, x, y, latitude, longitude);
+        }
+    }
+}
---
+++ Crime/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using crime;
+using System.ComponentModel;
+using System.IO;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Crime> crimes = new List<Crime>();
+        
+        using (var reader = new StreamReader("crime.csv"))
+        {
+            //List<string> listA = new List<string>();
+            //List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                Crime c1 = new Crime();
+
+                for (int i = 0; i < 12; i++)
+                {
+                    c1 = new Crime(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], values[10], values[11]);
+                }
+
+                crimes.Add(c1);
+                //year.Add(values[1]);
+            }
+        }
+
+        for (int i = 0; i < crimes.Count; i++)
+        {
+            Console.WriteLine(crimes[i]);
+        }
+        Console.WriteLine("halo");
+    }
+}
